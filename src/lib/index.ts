cd56152f9b8e07e503bb6e82bcd5ecc07251a67f
Amtr4x <amtr4x@gmail.com>
2024-12-10 21:06:20 -0500

    ADD isNotStored function to

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,10 @@
+import { eq } from 'drizzle-orm';
+import { db } from './server/db';
+import { shortlinks } from './server/db/schema';
+
 export const URL_LENGTH = 25;
+
+export function isNotStored(url: string): boolean {
+	const dbEntries = db.select().from(shortlinks).having(eq(shortlinks.short, url));
+	return !dbEntries;
+}
