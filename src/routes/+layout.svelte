c27e2a073f904bdcafb12a89bee4a88a3ebdebb6
Amtr4x <amtr4x@gmail.com>
2024-12-11 21:12:07 -0500

    ADD header to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,7 @@
 	let { children } = $props();
 </script>
 
+<h1 class="pt-4 text-center text-3xl font-bold">Minilink 🔗️</h1>
+<p class="text-center text-slate-800">Convert any large link in a short link with easy!</p>
+
 {@render children()}
