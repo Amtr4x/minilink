1f1b5008c87594e098b2a07080882a8a3e07d833
Amtr4x <amtr4x@gmail.com>
2024-12-12 15:39:11 -0500

    UPDATE metatags, title and favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,13 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Minilink</title>
+	<meta name="description" content="A minimalistic url shortener" />
+	<meta name="keywords" content="url-shortener urlshortener url shortner" />
+	<meta name="author" content="Amtr4x" />
+</svelte:head>
+
 <h1 class="pt-4 text-center text-3xl font-bold">Minilink 🔗️</h1>
 <p class="text-center text-slate-800">Convert any large link in a short link with easy!</p>
 
