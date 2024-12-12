7be2e74ce83074805320438c4ce888b350486b37
Amtr4x <amtr4x@gmail.com>
2024-12-11 19:22:30 -0500

    UPDATE the url import naming

--- src/routes/[...req]/+server.ts
+++ src/routes/[...req]/+server.ts
@@ -1,8 +1,8 @@
 import { db } from '$lib/server/db/index.js';
-import * as schema from '$lib/server/db/schema.js';
 import { error, redirect } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
 import { URL_LENGTH } from '$lib';
+import { urls } from '$lib/server/db/schema.js';
 
 /**
  * Redirect the user if the provided pathname inside
@@ -12,10 +12,7 @@ export async function GET({ url }) {
 	const requestedLink = url.pathname.toString().slice(1);
 
 	const obtainedLink = (
-		await db
-			.select({ long: schema.shortlinks.long })
-			.from(schema.shortlinks)
-			.where(eq(schema.shortlinks.short, requestedLink))
+		await db.select({ long: urls.long }).from(urls).where(eq(urls.short, requestedLink))
 	)[0];
 
 	if (obtainedLink) {
