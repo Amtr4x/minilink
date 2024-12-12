871b7ae444656072160166b012b30708db72dc9d
Amtr4x <amtr4x@gmail.com>
2024-12-12 12:26:17 -0500

    ADD CopyLinkBtn component

---
+++ src/components/CopyLinkBtn.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+
+	interface Props {
+		shortlink: string;
+	}
+
+	let { shortlink }: Props = $props();
+	let visible = $state(true);
+
+	onMount(() => {
+		setTimeout(() => (visible = false), 5000);
+	});
+</script>
+
+{#if visible}
+	<button
+		out:fade={{ duration: 300 }}
+		class="mt-4 h-14 rounded-md bg-green-300 p-1 text-[14px]"
+		onclick={() => navigator.clipboard.writeText(shortlink)}
+	>
+		The shortered url is: <span>{shortlink}</span>
+	</button>
+{/if}
